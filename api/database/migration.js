0c5d63f03c5fc93cbf9cb8ad75c69be15b335ba4
Everton Dias <evertonsrdias@gmail.com>
2024-05-18 13:49:06 -0300

    Melhorando migration

--- api/database/migration.js
+++ api/database/migration.js
@@ -2,6 +2,13 @@ const sqlite3 = require('sqlite3').verbose();
 
 const db = new sqlite3.Database('database/zabank_database.sqlite');
 
-db.run(`CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR, email VARCHAR, password VARCHAR, picture INTEGER)`);
+db.serialize(() => {
+    db.run(`CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR, email VARCHAR, password VARCHAR, picture INTEGER)`);
+
+    db.run(`
+        INSERT INTO users (name, email, password)
+        VALUES ('João Silva', 'joaosilva@gmail.com', 'senhasegura123')
+    `);
+});
 
 db.close();
