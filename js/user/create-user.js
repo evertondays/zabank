e58170d9af3707f5f5b1e50c67b40e24f9563222
Everton Dias <evertonsrdias@gmail.com>
2024-05-19 23:11:02 -0300

    Conectando login e criação de conta ao backend

--- js/user/create-user.js
+++ js/user/create-user.js
@@ -1,13 +1,15 @@
-document.getElementById('login-form').addEventListener('submit', function (e) {
+document.getElementById('create-user-form').addEventListener('submit', function (e) {
     e.preventDefault();
-    login();
+    createUser();
 });
 
-async function login() {
+function createUser() {
+    var name = document.getElementById('name-input').value;
     var email = document.getElementById('email-input').value;
     var password = document.getElementById('password-input').value;
 
-    axios.post('http://localhost:3333/api/login', {
+    axios.post('http://localhost:3333/api/user', {
+        name: name,
         email: email,
         password: password
     }).then((response) => {
